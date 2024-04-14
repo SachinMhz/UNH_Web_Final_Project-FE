e19fe33fc789014041fa20bf4a1257a193e7a970
SachinMhz <sachin.mhz.sm@gmail.com>
2024-04-14 16:31:36 -0400

    Replace with GOT book code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <!--Step 3 use the component-->
+    <MyHeader
+      bookName="Game of Thrones"
+      authorName="George RR Martins"
+      yourName="Write your Name"
+    />
+    <BookBox :books="books" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+// Step: 1 Import the components
+import MyHeader from "./components/MyHeader.vue";
+import BookBox from "./components/BookBox.vue";
+
+export default {
+  name: "App",
+
+  // Step 2: Register the components
+  components: {
+    MyHeader,
+    BookBox,
+  },
+  data() {
+    return {
+      books: [],
+    };
+  },
+  methods: {
+    //promoises
+    async fetchbooks() {
+      const res = await fetch("http://localhost:9523/books ");
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+  },
+  async created() {
+    // this.books = await this.fetchbooks();
+    this.books = [];
+  },
+
+  // data() => this.data books
+  // method => this.method => fetchbook
+  // life cycle hooks created() => call our method here
+};
+</script>
+
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  font-family: "Montserrat", sans-serif;
+}
+.container {
+  max-width: 400px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+div {
+  margin-bottom: 0.5em;
 }
 </style>
